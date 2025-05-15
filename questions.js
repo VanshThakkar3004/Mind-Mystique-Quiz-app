f6e0fd81258193b259c81ea4691ded31fa2441af
VanshThakkar3004 <115863042+VanshThakkar3004@users.noreply.github.com>
2025-05-15 11:20:28 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,111 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the Capital of France?",
+    answer: "Paris",
+    options: [
+      "Paris",
+      "Berlin",
+      "Madrid",
+      "London"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The Largest Desert in the World?",
+    answer: "Antartica Desert",
+    options: [
+      "Thar Desert",
+      "Antartica Desert",
+      "Gobi Desert",
+      "Great Indian Desert",
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which is the Largest Reptile in the World?",
+    answer: "Saltwater Crocodile",
+    options: [
+      "Nile Crocodile",
+      "Cuban Crocodile",
+      "Saltwater Crocodile",
+      "New Guinea crocodile",
+    ]
+  },
+  {
+    numb: 8,
+    question: "Who co-founded Microsoft alongside Bill Gates?",
+    answer: "Paul Allen",
+    options: [
+      "Steve Wozniak",
+      "Mark Zuckerberg",
+      "Paul Allen",
+      "Steve Jobs",
+    ]
+  },
+  {
+    numb: 9,
+    question: "Who was the first President of the United States?",
+    answer: "George Washington",
+    options: [
+      "Benjamin Franklin",
+      "George Washington",
+      "John Adams",
+      "Thomas Jefferson",
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the capital city of Australia?",
+    answer: "Canberra",
+    options: 
+    ["Melbourne",
+     "Brisbane",
+      "Adelaide",
+    "Canberra"]
+  },
+];
